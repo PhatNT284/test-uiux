8c37158077a7a71085d6a68a5e3c5a22f07a8b0c
PhatNT284 <phat280405@gmail.com>
2025-09-17 20:29:53 +0700

    first commit

---
+++ demo_uiux/src/components/Info/constants.js
@@ -0,0 +1,27 @@
+import ingredientIcon from "@icons/svgs/ingredientIcon.svg";
+import orderIcon from "@icons/svgs/orderIcon.svg";
+import personIcon from "@icons/svgs/personIcon.svg";
+import supportIcon from "@icons/svgs/supportIcon.svg";
+
+export const dataInfo = [
+  {
+    title: "Personalize Menu",
+    description: "Just for your taste",
+    src: personIcon,
+  },
+  {
+    title: "Fresh & Safe Ingredients",
+    description: "Daily selected",
+    src: ingredientIcon,
+  },
+  {
+    title: "Flexible Order",
+    description: "Dine-in ore take away",
+    src: orderIcon,
+  },
+  {
+    title: "24/7 Support",
+    description: "Away available",
+    src: supportIcon,
+  },
+];
