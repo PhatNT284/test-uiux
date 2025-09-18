7d42368485379865f92a5efa948c8cb565fcdddc
PhatNT284 <phat280405@gmail.com>
2025-09-18 22:41:08 +0700

    update code

---
+++ demo_uiux/src/apis/axiosClient.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: "https://api.example.com",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
