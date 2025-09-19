cc56a78dbc783be8b2257e4adaff91883eaa016c
PhatNT284 <phat280405@gmail.com>
2025-09-19 21:16:21 +0700

    update code

--- demo_uiux/src/App.jsx
+++ demo_uiux/src/App.jsx
@@ -1,10 +1,26 @@
 import HomePage from "@components/HomePage/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blog from "./components/Blog/Blog";
+import routers from "@/routers/routers";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {routers.map((item, index) => {
+            return (
+              <Route
+                path={item.path}
+                element={<item.component />}
+                key={index}
+              />
+            );
+          })}
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
